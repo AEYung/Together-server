236ecfdf66916309005613c9a45ae78e3af89b31
이은우 <80537533+lew0205@users.noreply.github.com>
2022-12-19 17:18:01 +0900

    return responseEntity

--- src/main/kotlin/com/aeyoung/together/domain/member/controller/AuthController.kt
+++ src/main/kotlin/com/aeyoung/together/domain/member/controller/AuthController.kt
@@ -2,7 +2,10 @@ package com.aeyoung.together.domain.member.controller
 
 import com.aeyoung.together.domain.member.dto.req.MemberSignUpReqDto
 import com.aeyoung.together.domain.member.service.req.MemberSignUpService
+import com.aeyoung.together.global.response.SuccessResponse
 import jakarta.validation.Valid
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -11,11 +14,11 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/members")
 class AuthController(
-    private val memberSignUpService: MemberSignUpService
+        private val memberSignUpService: MemberSignUpService
 ) {
 
     @PostMapping("/signup")
-    fun signUp(@Valid @RequestBody member: MemberSignUpReqDto): Long {
-        return memberSignUpService.join(member)
+    fun signUp(@Valid @RequestBody member: MemberSignUpReqDto): ResponseEntity<SuccessResponse> {
+        return ResponseEntity(SuccessResponse, HttpStatus.OK);
     }
 }
---
+++ src/main/kotlin/com/aeyoung/together/global/response/SuccessResponse.kt
@@ -0,0 +1,6 @@
+package com.aeyoung.together.global.response
+
+object SuccessResponse {
+    val msg: String = "성공했습니다"
+    val code: Int = 200
+}
