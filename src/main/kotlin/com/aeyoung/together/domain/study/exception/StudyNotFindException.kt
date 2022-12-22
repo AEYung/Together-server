8b7200724abfb37821c6b9b72bf8424f7f12db83
jeayoung <k01066624566@gmail.com>
2022-12-22 16:46:03 +0900

    exception 네이밍 수정

--- src/main/kotlin/com/aeyoung/together/domain/study/exception/StudyNotFindException.kt
+++ src/main/kotlin/com/aeyoung/together/domain/study/exception/StudyNotFindException.kt
@@ -3,5 +3,5 @@ package com.aeyoung.together.domain.study.exception
 import com.aeyoung.together.global.exception.BasicException
 import com.aeyoung.together.global.exception.ErrorCode
 
-class StudyNotFindException: BasicException(ErrorCode.STUDY_NOT_FOUND) {
+class StudyNotFoundException: BasicException(ErrorCode.STUDY_NOT_FOUND) {
 }
